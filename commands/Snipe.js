1781bd8546f2179b7e4bfbb7b49db7965eacf57b
i-yoshiaki <yosshy79iy@gmail.com>
2021-12-30 00:44:37 +0900

    2021/12/30 スナイプコマンド追加

---
+++ commands/Snipe.js
@@ -0,0 +1,30 @@
+module.exports = {
+    data: {
+        name: "snipe",
+        description: "スナイプするときに使います。10秒後に返信します。",
+    },
+    async execute(interaction) {
+        const wait = require('util').promisify(setTimeout);
+        await interaction.reply('10');
+        await wait(1000);
+        await interaction.editReply('9');
+        await wait(1000);
+        await interaction.editReply('8');
+        await wait(1000);
+        await interaction.editReply('7');
+        await wait(1000);
+        await interaction.editReply('6');
+        await wait(1000);
+        await interaction.editReply('5');
+        await wait(1000);
+        await interaction.editReply('4');
+        await wait(1000);
+        await interaction.editReply('3');
+        await wait(1000);
+        await interaction.editReply('2');
+        await wait(1000);
+        await interaction.editReply('1');
+        await wait(1000);
+        await interaction.editReply('Go!');
+    }
+}
