e6d29d28689897fd00e480587af2998a4d45f43a
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-09 14:15:00 +0900

    test2

--- commands/id.js
+++ commands/id.js
@@ -5,9 +5,10 @@ module.exports = {
     },
     async execute(interaction) {
         //環境変数読み込み
-        // require('dotenv').config();
-        // const serverId = process.env.SERVER_ID;
-        // const defaultChannelId = process.env.DEFAULT_CHANNEL_ID;
-        await interaction.reply('id');
+        require('dotenv').config();
+        const serverId = process.env.SERVER_ID;
+        const defaultChannelId = process.env.DEFAULT_CHANNEL_ID;
+        await interaction.reply(serverId);
+        await interaction.followUp(defaultChannelId);
     }
 }
