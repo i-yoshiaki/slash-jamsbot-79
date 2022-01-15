f067fc9641f26612ae5d0909710eb300e832b181
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-16 04:01:30 +0900

    2022/01/16 sqlテスト

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 exports.pool = new pg.Pool ({
     connectionString:process.env.DATABASE_URL,
-    dialect: 'postgres',
     dialectOptions: {
         ssl: {
           require: true,
