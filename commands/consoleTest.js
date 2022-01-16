5b5db885eaa35f6c80660573bbd8ca0e7c7deb18
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-16 17:34:05 +0900

    2022/01/16 データベースバロラントテスト

--- commands/consoleTest.js
+++ commands/consoleTest.js
@@ -4,9 +4,13 @@ module.exports = {
       description: "console見る用",
   },
   async execute(interaction) {
-      const birthday = require('../birthday.js');
-      let embed = birthday.birthdayExecute();
-      await interaction.reply({ embeds: [embed] });
-      
-  }
+    const db = require('../db.js');
+    let tableResult;
+    db.pool.connect()
+    .then(() => console.log("Connected successfuly"))
+    .then(() => db.pool.query("SELECT * FROM ValorantAgentTable"))
+    .then(result => tableResult = result.rows[0])
+    .then(() => console.log(tableResult.url))
+    .then(() => interaction.reply("tableResult = "+tableResult.url))
+}
 }
