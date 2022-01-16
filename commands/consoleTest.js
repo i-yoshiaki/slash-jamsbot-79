403d035a72f3f49f6fe4ac6803e3b2a74c73dc28
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-17 00:52:15 +0900

    2022/1/17

--- commands/consoleTest.js
+++ commands/consoleTest.js
@@ -19,13 +19,13 @@ module.exports = {
     const shamojiv = new Date(process.env.SHAMOJI_BIRTHDAY.slice(0,4),process.env.SHAMOJI_BIRTHDAY.slice(4,6),process.env.SHAMOJI_BIRTHDAY.slice(6,8));
     const tasunv = new Date(process.env.TASUN_BIRTHDAY.slice(0,4),process.env.TASUN_BIRTHDAY.slice(4,6),process.env.TASUN_BIRTHDAY.slice(6,8));
     const machamiv = new Date(process.env.MACHAMI_BIRTHDAY.slice(0,4),process.env.MACHAMI_BIRTHDAY.slice(4,6),process.env.MACHAMI_BIRTHDAY.slice(6,8));
-    console.log(today)
-    console.log(machamiv)
-    console.log(today.getDate())
-    console.log(machamiv.getDate())
-    console.log(today.getMonth())
-    console.log(machamiv.getMonth())
+    console.log("today"=today)
+    console.log("machamiv"=machamiv)
+    console.log("todayd"=today.getDate())
+    console.log("machamivd"=machamiv.getDate())
+    console.log("todaym"=today.getMonth())
+    console.log("machamivm"=machamiv.getMonth())
     await interaction.reply("test");
-    
+
   }
 }
