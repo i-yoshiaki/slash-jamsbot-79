4d39ee948cfd3ce684c30543ac7276a6b9f472f6
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-18 23:05:43 +0900

    クエリ系追加

---
+++ commands/tableQuery.js
@@ -0,0 +1,28 @@
+module.exports = {
+    data: {
+        name: "query-execute",
+        description: "【Admin】クエリ実行",
+        options: [
+            {
+                type: "STRING",
+                name: "query",
+                description: "query入力",
+                required: true,
+            }
+        ],
+    },
+    async execute(interaction) {
+        const db = require('../db.js');
+        const admin = require('../function/adminCheck.js');
+        const queryString=interaction.options.getString('query');
+        let str="";
+        let adminFlg=admin.adminCheckExecute(interaction);
+        if(adminFlg){
+            let tableResult;
+            db.pool.connect()
+            .then(() => console.log("Connected successfuly"))
+            .then(() => db.pool.query(queryString))
+            .then(() =>interaction.reply("クエリ実行完了"))
+        }else await interaction.reply("権限がないよ")
+    }
+}
