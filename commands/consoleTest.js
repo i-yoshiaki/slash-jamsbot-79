e68812ffd9db88c4dee028bb4d7614566aec8bbf
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-18 22:26:55 +0900

    agent-list追加

--- commands/consoleTest.js
+++ commands/consoleTest.js
@@ -4,22 +4,6 @@ module.exports = {
       description: "console見る用",
   },
   async execute(interaction) {
-      const db = require('../db.js');
-      const admin = require('../function/adminCheck.js');
-      let str="";
-      let adminFlg=admin.adminCheckExecute(interaction);
-      if(adminFlg){
-          let tableResult;
-          db.pool.connect()
-          .then(() => console.log("Connected successfuly"))
-          .then(() => db.pool.query("SELECT * FROM ValorantAgentTable ORDER BY id ASC"))
-          .then(result => tableResult = result.rows)
-          .then(function(){
-              for(let i=0;i<tableResult.length;i++){
-                  str+=tableResult[i].name+"\n"
-              }
-              interaction.reply(str);
-          })
-      }else await interaction.reply("権限がないよ")
+      await interaction.reply("テスト");
   }
 }
