9921f4c89c8dc9a6083efafa81f4c50ee15dcfda
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-20 02:13:20 +0900

    2022/01/20 dbpoolリリース

--- commands/02randomAgent.js
+++ commands/02randomAgent.js
@@ -21,6 +21,17 @@ module.exports = {
     async execute(interaction) {
         //*query文字列
         let sql="";
+        if(interaction.options.getString('category') === null || interaction.options.getString('category') === "0"){
+            sql="SELECT * FROM valorantagenttable";
+        }else if(interaction.options.getString('category') === "1"){
+            sql="SELECT * FROM valorantagenttable where category = 1";
+        }else if(interaction.options.getString('category') === "2"){
+            sql="SELECT * FROM valorantagenttable where category = 2";
+        }else if(interaction.options.getString('category') === "3"){
+            sql="SELECT * FROM valorantagenttable where category = 3";
+        }else if(interaction.options.getString('category') === "4"){
+            sql="SELECT * FROM valorantagenttable where category = 4";
+        }
         //*変数初期化
         let tableResult = null;
         let agentNum = 0;
@@ -28,39 +39,36 @@ module.exports = {
         let agentUrl = "";
         //*db接続
         const db = require('../db.js');
+
         db.pool.connect()
-        .then(() => console.log("Connected successfuly"))
-        .then(function(){
-            if(interaction.options.getString('category') === null || interaction.options.getString('category') === "0"){
-                sql="SELECT * FROM valorantagenttable";
-            }else if(interaction.options.getString('category') === "1"){
-                sql="SELECT * FROM valorantagenttable where category = 1";
-            }else if(interaction.options.getString('category') === "2"){
-                sql="SELECT * FROM valorantagenttable where category = 2";
-            }else if(interaction.options.getString('category') === "3"){
-                sql="SELECT * FROM valorantagenttable where category = 3";
-            }else if(interaction.options.getString('category') === "4"){
-                sql="SELECT * FROM valorantagenttable where category = 4";
-            }
-        })
-        .then(() => db.pool.query(sql))
-        .then(result => tableResult = result.rows)
-        .then(function(){
-            agentNum=Math.floor(Math.random()*(tableResult.length-0))+0;
-            agentName = tableResult[agentNum].name
-            agentUrl = tableResult[agentNum].url;
-            const embed = {
-                //!タイトル
-                "title": agentName+"に決まりました。",
-                //!色
-                "color": 6697983,
-                //!大きい画像
-                "image": {
-                    "url": agentUrl
+        .then(client => {
+            return client
+            .query(sql)
+            .then(result => {
+                client.release()
+                tableResult = result.rows
+                console.log(tableResult);
+            })
+            .catch(err => {
+                client.release()
+                console.log(err.stack)
+            })
+            .then(function(){
+                agentNum=Math.floor(Math.random()*(tableResult.length-0))+0;
+                agentName = tableResult[agentNum].name
+                agentUrl = tableResult[agentNum].url;
+                const embed = {
+                    //!タイトル
+                    "title": agentName+"に決まりました。",
+                    //!色
+                    "color": 6697983,
+                    //!大きい画像
+                    "image": {
+                        "url": agentUrl
+                    }
                 }
-            }
-            interaction.reply({ embeds: [embed] });
+                interaction.reply({ embeds: [embed] });
+            })
         })
-        .catch(console.error)
     }
 }
