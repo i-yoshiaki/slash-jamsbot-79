bd8773a08d932ea1b9eeb0e7ab7d04f1ecf24a3e
yoshiaki-iseki <yosshy79iy@gmail.com>
2022-01-19 23:32:35 +0900

    2022/01/19 コンソールテスト権限

--- commands/99consoleTest.js
+++ commands/99consoleTest.js
@@ -1,9 +1,13 @@
 module.exports = {
   data: {
       name: "99-console-test",
-      description: "console見る用",
+      description: "【Admin】console見る用",
   },
   async execute(interaction) {
-      await interaction.reply("テスト");
-  }
+      const admin = require('../function/adminCheck.js');
+      let adminFlg=admin.adminCheckExecute(interaction);
+      if(adminFlg){
+        await interaction.reply("テスト");
+      }else await interaction.reply("権限がないよ")
+  } 
 }
